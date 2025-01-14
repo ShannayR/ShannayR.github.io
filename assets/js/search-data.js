268ac330ddfbe0d1f9d8b6cbc48761224e1e3479
ShannayR <51871995+ShannayR@users.noreply.github.com>
2025-01-14 00:42:07 +0000

    Deploying to gh-pages from @ ShannayR/ShannayR.github.io@4bf421649e824865b00d65029868d61335847db6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,21 +492,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%63%6F%6E%74%61%63%74@%73%68%61%6E%6E%61%79%72%61%77%61%6C.%63%6F%6D", "_blank");
+          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-orcid',
-        title: 'ORCID',
+        id: 'social-inspire',
+        title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
-          window.open("https://orcid.org/0000-0002-0714-0618", "_blank");
-        },
-      },{
-        id: 'social-researchgate',
-        title: 'ResearchGate',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.researchgate.net/profile/https://www.researchgate.net/profile/Shannay-Rawal/", "_blank");
+          window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
         id: 'social-rss',
@@ -520,7 +513,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=N1QfuysAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-custom_social',
+        title: 'Custom_social',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
