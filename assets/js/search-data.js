68c5933f408f37221d0070a31d0981cba0759e26
ShannayR <51871995+ShannayR@users.noreply.github.com>
2025-01-28 00:40:51 +0000

    Deploying to gh-pages from @ ShannayR/ShannayR.github.io@5f52ce259bb0d17898f152a657d8e093d5adfb91 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -94,56 +94,21 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-data-management-and-security-for-student-researchers",
+          section: "News",},{id: "projects-data-management-and-security-for-student-researchers",
           title: 'Data Management and Security for Student Researchers',
           description: "CITI Training",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/citi_1/";
+              window.location.href = "/projects/citi_DataMgt-Sec/";
+            },},{id: "projects-responsible-conduct-of-research",
+          title: 'Responsible Conduct of Research',
+          description: "CITI Training",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/citi_RCR/";
+            },},{id: "projects-research-investigators-human-subject-research",
+          title: 'Research Investigators (Human Subject Research)',
+          description: "CITI Training",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/citi_RI_5yr/";
             },},{
         id: 'social-bluesky',
         title: 'Bluesky',
