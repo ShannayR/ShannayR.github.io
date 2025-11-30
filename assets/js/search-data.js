ed48c5f583573d7fb7691b7b1f27014aca347e86
ShannayR <51871995+ShannayR@users.noreply.github.com>
2025-11-30 00:09:33 +0000

    Deploying to gh-pages from @ ShannayR/ShannayR.github.io@84fa0514e0a2ed5c09600ef18edaadfcc7b54a91 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Courses taught at WVU",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
